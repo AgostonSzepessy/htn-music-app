1d94c55a8e93817faffa8bdf9b60a8c24c4661e3
Megan Hasegawa <meganhasegawa@v1020-wn-195-133.campus-dynamic.uwaterloo.ca>
2016-09-17 14:36:38 -0400

    Sign in.

--- app/src/main/java/western/musicapp/StartupActivity.java
+++ app/src/main/java/western/musicapp/StartupActivity.java
@@ -2,12 +2,43 @@ package western.musicapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import com.firebase.ui.auth.AuthUI;
 
 public class StartupActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+
+        startActivityForResult(
+                // Get an instance of AuthUI based on the default app
+                AuthUI.getInstance().createSignInIntentBuilder().build(),
+                RC_SIGN_IN);
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setProviders(
+                                AuthUI.GOOGLE_PROVIDER)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // user is signed in!
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            } else {
+                // user is not signed in. Maybe just wait for the user to press
+                // "sign in" again, or show a message
+            }
+        }
     }
 }
