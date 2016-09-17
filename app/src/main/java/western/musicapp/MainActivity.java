0b89c3b9cf85a724e160c406e2201840f5938f56
gpadmaku1 <gpadmaku@gmail.com>
2016-09-17 09:34:15 -0400

    xml code and changed UI a bit
    Updated xml files to get rid of title bar and changed layouts

--- app/src/main/java/western/musicapp/MainActivity.java
+++ app/src/main/java/western/musicapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,14 +14,17 @@ public class MainActivity extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
 
-	}
+    }
 
 	public void switchToHost(View view){
 
-		Intent hostIntent = new Intent(this, GuestActivity.class);
-        startActivity(hostIntent);
+		//Intent hostIntent = new Intent(this, HostActivity.class);
+        //startActivity(hostIntent);
 
 	}
 
