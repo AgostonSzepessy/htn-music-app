274aa09dbda361653a0dfd0bd2e21ad1a33156a2
gpadmaku1 <gpadmaku@gmail.com>
2016-09-17 03:40:40 -0400

    switchToHost function
    Wrote a function to switch between activities main and host.

--- app/src/main/java/western/musicapp/MainActivity.java
+++ app/src/main/java/western/musicapp/MainActivity.java
@@ -1,7 +1,9 @@
 package western.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,6 +14,12 @@ public class MainActivity extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+	}
+
+	public void switchToHost(View view){
+
+		Intent hostIntent = new Intent(this, HostActivity.class);
+        startActivity(hostIntent);
 
 	}
 }
